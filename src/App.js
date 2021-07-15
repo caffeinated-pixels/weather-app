31ef0b12cfd8748e59fb7395d4bbc74014cec982
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-15 11:37:54 -0400

    add Searchbar & CurrentWeather components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import SearchBar from './components/SearchBar'
+import CurrentWeather from './components/CurrentWeather'
 
 export default function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <SearchBar />
+      <CurrentWeather />
+    </>
+  )
 }
---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function CurrentWeather() {
+  return <h2>Current weather</h2>
+}
