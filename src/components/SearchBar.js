20988c8dd27459b171d0dbf2a6c63c08a9c5ec3c
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-15 17:18:56 -0400

    Add Font Awesome plus location & search icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,5 +4,11 @@ import { WeatherDataContext } from '../WeatherDataContext'
 export default function SearchBar() {
   const { locationData } = useContext(WeatherDataContext)
 
-  return <h2>{locationData}</h2>
+  return (
+    <>
+      <i className="fas fa-location-arrow"></i>
+      <h2>{locationData}</h2>
+      <i className="fas fa-search-location"></i>
+    </>
+  )
 }
