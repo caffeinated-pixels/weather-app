cd9a482f2be375f7cf76df668db2cbe1c4fa74c6
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-16 14:15:01 -0400

    Expand time conditional logic to display variety of values

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,11 +1,10 @@
 import React, { useContext } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
+import { monthLookup } from '../helpers/timeLookup'
 
 export default function HourlyForecast() {
   const { weatherData, isLoading } = useContext(WeatherDataContext)
   let hourlyBoxes
-
-  const date = new Date()
 
   if (isLoading) {
     hourlyBoxes = <p>still loading</p>
@@ -13,24 +12,34 @@ export default function HourlyForecast() {
     hourlyBoxes = weatherData.hourly.map((hourly, i) => {
       const backgroundColor = i % 2 === 0 ? 'hourly-light' : 'hourly-dark'
       const date = new Date(hourly.dt * 1000)
+      const hour = date.getHours()
+
+      // Display 'Now' for current hour, display date if midnight (i.e. new day), else display the actual hour in hh:00 format
+      const hourDisplay =
+        i === 0
+          ? 'Now'
+          : hour === 0
+          ? `${monthLookup[date.getMonth()]} ${date.getDate()}`
+          : `${hour}:00`
 
       // TODO: convert to location timezone; the time will be in the user's local time (so the location timezone may be different!!!)
-      const hour = i === 0 ? 'Now' : `${date.getHours()}:00`
 
       return (
         <div key={`hourly-${i}`} className={`hourly-box ${backgroundColor}`}>
-          <p className="hourly-time">{hour}</p>
-          <img
-            src="https://openweathermap.org/img/wn/10d@2x.png"
-            alt="weather icon"
-          />
+          <p className="hourly-time">{hourDisplay}</p>
+          <div className="hourly-icon-wrapper">
+            <img
+              className="hourly-icon"
+              src="https://openweathermap.org/img/wn/10d@2x.png"
+              alt="weather icon"
+            />
+          </div>
+
           <p className="hourly-temp">{Math.round(hourly.temp)}°C</p>
         </div>
       )
     })
   }
-
-  // console.log(hourlyBoxes)
 
   return <section className="hourly-forecast-ribbon">{hourlyBoxes}</section>
 }
---
+++ src/helpers/timeLookup.js
@@ -0,0 +1,16 @@
+const monthLookup = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+
+export { monthLookup }
