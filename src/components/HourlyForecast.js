c3a30b2152c72251f591362db110ac6dd9a0b0db
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-16 12:38:24 -0400

    add time logic to convert unix into local hours (user's timezone)

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -12,10 +12,14 @@ export default function HourlyForecast() {
   } else {
     hourlyBoxes = weatherData.hourly.map((hourly, i) => {
       const backgroundColor = i % 2 === 0 ? 'hourly-light' : 'hourly-dark'
+      const date = new Date(hourly.dt * 1000)
+
+      // TODO: convert to location timezone; the time will be in the user's local time (so the location timezone may be different!!!)
+      const hour = i === 0 ? 'Now' : `${date.getHours()}:00`
 
       return (
         <div key={`hourly-${i}`} className={`hourly-box ${backgroundColor}`}>
-          <p className="hourly-time">{date.getHours() + i}:00</p>
+          <p className="hourly-time">{hour}</p>
           <img
             src="https://openweathermap.org/img/wn/10d@2x.png"
             alt="weather icon"
