79fe1a68876399a1080fae9e86fa41a9991c4517
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-16 08:14:22 -0400

    Style LocationBar

--- src/components/LocationBar.js
+++ src/components/LocationBar.js
@@ -6,10 +6,10 @@ export default function LocationBar() {
 
   return (
     <div className="locationbar">
-      <i className="fas fa-location-arrow"></i>
+      <i className="locationbar__icon fas fa-location-arrow"></i>
       <div className="locationbar__city">{locationData}</div>
-      <i className="fas fa-search-location"></i>
-      <i class="fas fa-caret-square-down"></i>
+      <i className="locationbar__icon fas fa-search-location"></i>
+      <i class="locationbar__icon fas fa-caret-square-down"></i>
     </div>
   )
 }
