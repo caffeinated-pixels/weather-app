e4350e278565fb5e82832c644fe11f8d2a7d735e
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-16 08:01:20 -0400

    Rename SearchBar to LocationBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
-import SearchBar from './components/SearchBar'
+import LocationBar from './components/LocationBar'
 import CurrentWeather from './components/CurrentWeather'
 
 export default function App() {
   return (
     <>
       <Header />
-      <SearchBar />
+      <LocationBar />
       <CurrentWeather />
     </>
   )
--- src/components/LocationBar.js
+++ src/components/LocationBar.js
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
 
-export default function SearchBar() {
+export default function LocationBar() {
   const { locationData } = useContext(WeatherDataContext)
 
   return (
-    <>
+    <div className="locationbar">
       <i className="fas fa-location-arrow"></i>
-      <h2>{locationData}</h2>
+      <div className="locationbar__city">{locationData}</div>
       <i className="fas fa-search-location"></i>
-    </>
+      <i class="fas fa-caret-square-down"></i>
+    </div>
   )
 }
