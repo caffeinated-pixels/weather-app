4b93737b7252375cd0b6ec60869e08878d239fbf
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-19 14:43:59 -0400

    Change fa icon class attr to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>
-        coded by Stevie Gill | <i class="fab fa-github-square"></i> Repo
+        coded by Stevie Gill | <i className="fab fa-github-square"></i> Repo
       </p>
     </footer>
   )
