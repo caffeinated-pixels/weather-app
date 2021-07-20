00b63c1f30f9808cf1d7cc8f548025f9531d3372
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-20 11:18:50 -0400

    Add fn for getting user location

--- src/WeatherDataContext.js
+++ src/WeatherDataContext.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import getUserLocation from './helpers/getUserLocation'
 
 const WeatherDataContext = React.createContext()
+getUserLocation()
 
 function WeatherDataContextProvider({ children }) {
   const [isLoading, setIsLoading] = useState(true)
