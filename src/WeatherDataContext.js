2eb300235fb3a3e01d011c86e552253a8fd3e6a3
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-20 15:07:52 -0400

    Set user location to state object

--- src/WeatherDataContext.js
+++ src/WeatherDataContext.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import getUserLocation from './helpers/getUserLocation'
+// import getUserLocation from './helpers/getUserLocation'
 
 const WeatherDataContext = React.createContext()
-getUserLocation()
 
 function WeatherDataContextProvider({ children }) {
   const [isLoading, setIsLoading] = useState(true)
+  const [locationData, setLocation] = useState({})
   const [weatherData, setWeatherData] = useState({})
-  const [locationData] = useState('Toronto, CA')
 
   // const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall'
   // const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
@@ -17,32 +16,59 @@ function WeatherDataContextProvider({ children }) {
   //
   // const fullUrl = `${baseUrl}?lat=${lat}&lon=${lon}&units=${units}&APPID=${apiKey}`
 
-  // useEffect(() => {
-  //   fetch('./weatherDataPlaceholder.json')
-  //     .then(response => response.json())
-  //     .then(data => processWeatherData(data))
-  //     .catch(error => console.log(error))
-  // }, [])
-
-  useEffect(() => {
-    async function fetchItems() {
-      try {
-        const response = await fetch('./weatherDataPlaceholder.json')
-
-        if (!response.ok)
-          throw Error('Fetch request to Open Weather API failed')
-
-        const weatherData = await response.json()
-        setWeatherData(weatherData)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        // NOTE: this will execute last regardless of try/catch outcome, so may need to refactor for dealing with fetch errors
-        setIsLoading(false)
-      }
+  async function getUserLocation() {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
     }
 
-    setTimeout(() => fetchItems(), 1000)
+    function success(pos) {
+      const userCoords = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+      }
+
+      setLocation(userCoords)
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`)
+    }
+
+    try {
+      const pos = await new Promise((resolve, reject) =>
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+      )
+      success(pos)
+    } catch (err) {
+      console.log(error(err))
+    }
+  }
+
+  async function fetchWeatherData() {
+    try {
+      const response = await fetch('./weatherDataPlaceholder.json')
+
+      if (!response.ok) throw Error('Fetch request to Open Weather API failed')
+
+      const weatherData = await response.json()
+      setWeatherData(weatherData)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      // NOTE: this will execute last regardless of try/catch outcome, so may need to refactor for dealing with fetch errors
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    async function callAPIs() {
+      await getUserLocation()
+      setTimeout(() => fetchWeatherData(), 1000)
+    }
+
+    callAPIs()
   }, [])
 
   return (
--- src/components/LocationBar.js
+++ src/components/LocationBar.js
@@ -3,11 +3,12 @@ import { WeatherDataContext } from '../WeatherDataContext'
 
 export default function LocationBar() {
   const { locationData } = useContext(WeatherDataContext)
+  console.log(locationData)
 
   return (
     <div className="locationbar">
       <i className="locationbar__icon fas fa-location-arrow"></i>
-      <div className="locationbar__city">{locationData}</div>
+      <div className="locationbar__city">Toronto, CA</div>
       <i className="locationbar__icon fas fa-search-location"></i>
       <i className="locationbar__icon fas fa-caret-square-down"></i>
     </div>
