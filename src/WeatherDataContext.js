a55b4ae90960f979e95a54e3f49676c8a712085e
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-22 12:55:59 -0400

    Create useFetchWeatherData hook

--- src/WeatherDataContext.js
+++ src/WeatherDataContext.js
@@ -1,48 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import useGeolocation from './hooks/useGeolocation'
-// import getUserLocation from './helpers/getUserLocation'
+import useFetchWeatherData from './hooks/useFetchWeatherData'
 
 const WeatherDataContext = React.createContext()
 
 function WeatherDataContextProvider({ children }) {
   const [locationData, getUserLocation] = useGeolocation()
-  const [isLoading, setIsLoading] = useState(true)
-  // const [locationData, setLocation] = useState({})
-  const [weatherData, setWeatherData] = useState({})
-
-  // console.log(`context hook call: ${location.latitude}`)
-
-  // const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall'
-  // const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
-  // const lat = '43.651070'
-  // const lon = '-79.347015'
-  // const units = 'metric'
-  //
-  // const fullUrl = `${baseUrl}?lat=${lat}&lon=${lon}&units=${units}&APPID=${apiKey}`
-
-  // const getUserLocation = useCallback(() => {
-  //   const options = {
-  //     enableHighAccuracy: true,
-  //     timeout: 5000,
-  //     maximumAge: 0
-  //   }
-  //
-  //   function success(pos) {
-  //     const userCoords = {
-  //       latitude: pos.coords.latitude,
-  //       longitude: pos.coords.longitude
-  //     }
-  //
-  //     setLocation(userCoords)
-  //     fetchWeatherData(userCoords)
-  //   }
-  //
-  //   function error(err) {
-  //     console.warn(`ERROR(${err.code}): ${err.message}`)
-  //   }
-  //
-  //   navigator.geolocation.getCurrentPosition(success, error, options)
-  // }, [])
+  const [{ weatherData, isLoading }, fetchWeatherData] = useFetchWeatherData({
+    latitude: 43.7001,
+    longitude: -79.4163
+  })
 
   useEffect(() => {
     console.log('1st context use effect')
@@ -52,34 +19,10 @@ function WeatherDataContextProvider({ children }) {
   useEffect(() => {
     console.log('2nd context use effect')
 
-    async function fetchWeatherData() {
-      console.log('API call')
-      const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall'
-      const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
-      const units = 'metric'
-
-      const fullUrl = `${baseUrl}?lat=${locationData.latitude}&lon=${locationData.longitude}&units=${units}&APPID=${apiKey}`
-
-      try {
-        const response = await fetch(fullUrl)
-
-        if (!response.ok)
-          throw Error('Fetch request to Open Weather API failed')
-
-        const weatherData = await response.json()
-        setWeatherData(weatherData)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        // NOTE: this will execute last regardless of try/catch outcome, so may need to refactor for dealing with fetch errors
-        setIsLoading(false)
-      }
-    }
-
     if (locationData.latitude) {
-      fetchWeatherData()
+      fetchWeatherData(locationData)
     }
-  }, [locationData])
+  }, [locationData, fetchWeatherData])
 
   return (
     <WeatherDataContext.Provider
---
+++ src/hooks/useFetchWeatherData.js
@@ -0,0 +1,34 @@
+import { useState, useCallback } from 'react'
+
+export default function useFetchWeatherData() {
+  const [weatherData, setWeatherData] = useState({
+    weatherData: {},
+    isLoading: true
+  })
+
+  const fetchWeatherData = useCallback(
+    async locationData => {
+      console.log('API call')
+      const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall'
+      const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
+      const units = 'metric'
+
+      const fullUrl = `${baseUrl}?lat=${locationData.latitude}&lon=${locationData.longitude}&units=${units}&APPID=${apiKey}`
+
+      try {
+        const response = await fetch(fullUrl)
+
+        if (!response.ok)
+          throw Error('Fetch request to Open Weather API failed')
+
+        const weatherData = await response.json()
+        setWeatherData({ weatherData, isLoading: false })
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    [setWeatherData]
+  )
+
+  return [weatherData, fetchWeatherData]
+}
