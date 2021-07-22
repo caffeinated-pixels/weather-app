6343f14a49e566b299e1e8e6129edd8ad1c2c7de
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-22 14:19:29 -0400

    Add geocoding API call to useBrowserGeolocation

--- src/components/LocationBar.js
+++ src/components/LocationBar.js
@@ -3,12 +3,15 @@ import { WeatherDataContext } from '../WeatherDataContext'
 
 export default function LocationBar() {
   const { locationData } = useContext(WeatherDataContext)
+  const { city, country } = locationData
   console.log(`location Bar log: ${locationData.latitude}`)
 
   return (
     <div className="locationbar">
       <i className="locationbar__icon fas fa-location-arrow"></i>
-      <div className="locationbar__city">Toronto, CA</div>
+      <div className="locationbar__city">
+        {city}, {country}
+      </div>
       <i className="locationbar__icon fas fa-search-location"></i>
       <i className="locationbar__icon fas fa-caret-square-down"></i>
     </div>
--- src/hooks/useBrowserGeolocation.js
+++ src/hooks/useBrowserGeolocation.js
@@ -3,7 +3,7 @@ import { useState, useCallback } from 'react'
 export default function useBrowserGeolocation() {
   const [userLocation, setLocation] = useState({})
 
-  const getBrowserGeolocation = useCallback(async () => {
+  const getBrowserGeolocation = useCallback(() => {
     console.log('get getCurrentPosition call')
     const options = {
       enableHighAccuracy: true,
@@ -11,13 +11,22 @@ export default function useBrowserGeolocation() {
       maximumAge: 0
     }
 
-    function success(pos) {
-      const userCoords = {
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude
-      }
+    async function success(pos) {
+      const latitude = pos.coords.latitude
+      const longitude = pos.coords.longitude
 
-      setLocation(userCoords)
+      const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
+      const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+
+      try {
+        const response = await fetch(url)
+        const geocodingResult = await response.json()
+        const city = geocodingResult[0].name
+        const country = geocodingResult[0].country
+        setLocation({ latitude, longitude, city, country })
+      } catch (error) {
+        console.log(`Geocoding error: ${error}`)
+      }
     }
 
     function error(err) {
