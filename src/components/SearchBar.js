9482e8fa962ac11ac9793501a78f408c961d7681
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-07-27 15:47:58 -0400

    Add text input and handleSubmit for search query

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
 
 export default function SearchBar() {
   const { locationName, getBrowserGeolocation } = useContext(WeatherDataContext)
   const { city, country } = locationName
+  const [searchInput, setSearchInput] = useState(`${city}, ${country}`)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('submit search input')
+  }
 
   return (
     <>
@@ -11,9 +17,14 @@ export default function SearchBar() {
         className="locationbar__icon fas fa-map-marker-alt"
         onClick={getBrowserGeolocation}
       ></i>
-      <div className="locationbar__city">
-        {city}, {country}
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="locationbar__city"
+          value={searchInput}
+          onChange={e => setSearchInput(e.target.value)}
+        />
+      </form>
       <i className="locationbar__icon fas fa-search-location"></i>
     </>
   )
