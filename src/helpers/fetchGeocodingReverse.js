51bfdfc81e7fc5854010cf3092d2e62cbdc4dd09
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-08-05 13:22:39 -0400

    Switch from fetch to axios for API calling

--- src/helpers/fetchGeocodingReverse.js
+++ src/helpers/fetchGeocodingReverse.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 /* In 'reverse' mode, we can query the Open Weather Geocoding API with lat & lon
 coords to get the city name & country. It will return an array of results
 (by default 1 but up to 5), but the first result should always be right. */
@@ -5,18 +7,24 @@ coords to get the city name & country. It will return an array of results
 export default async function fetchGeocodingAPI({ latitude, longitude }) {
   console.log('fetch geocoding reverse API helper')
   const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
-  const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+  const url = `ttps://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
 
   try {
-    const response = await fetch(url)
+    const response = await axios.get(url)
+    const geocodingResult = response.data
 
-    if (!response.ok) throw Error('Fetch request to Geocoding API failed')
-
-    const geocodingResult = await response.json()
     const city = geocodingResult[0].name
     const country = geocodingResult[0].country
     return [city, country]
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    if (err.response) {
+      // Not in the 200 response range
+      console.log(err.response.data)
+      console.log(err.response.status)
+      console.log(err.response.headers)
+    } else {
+      console.log(`Geocoding Reverse Error: ${err.message}`)
+      return ['', '']
+    }
   }
 }
