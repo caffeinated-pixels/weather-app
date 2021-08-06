169a574e636e06786999537cc9af3c773253750c
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-08-06 12:33:29 -0400

    Add custom scrollbars to hourly/daily

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,9 +1,15 @@
 import React, { useContext } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
+import SimpleBar from 'simplebar-react'
+import 'simplebar/dist/simplebar.min.css'
 
 export default function DailyForecast() {
   const { processedWeatherData } = useContext(WeatherDataContext)
   const { dailyBoxes } = processedWeatherData
 
-  return <section className="daily-forecast">{dailyBoxes}</section>
+  return (
+    <section className="daily-forecast">
+      <SimpleBar style={{ height: '100%' }}>{dailyBoxes}</SimpleBar>
+    </section>
+  )
 }
--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
+import SimpleBar from 'simplebar-react'
+import 'simplebar/dist/simplebar.min.css'
 
 export default function HourlyForecast() {
   const { processedWeatherData } = useContext(WeatherDataContext)
 
   const { hourlyBoxes } = processedWeatherData
 
-  return <section className="hourly-forecast-ribbon">{hourlyBoxes}</section>
+  return (
+    <section className="hourly-forecast-ribbon">
+      <SimpleBar>{hourlyBoxes}</SimpleBar>
+    </section>
+  )
 }
