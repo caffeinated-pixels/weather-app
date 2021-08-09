c955426a8481eb5f43b92ac47591ebfa407619e9
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-08-09 11:03:05 -0400

    Add tabIndex to SimpleBar scrollableNode

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
 
 export default function DailyForecast() {
   const { processedWeatherData } = useContext(WeatherDataContext)
+  const scrollableNodeRef = useRef(null)
+
   const { dailyBoxes } = processedWeatherData
+
+  useEffect(() => (scrollableNodeRef.current.tabIndex = '0'), [])
 
   return (
     <section className="daily-forecast">
-      <SimpleBar style={{ height: '100%' }}>{dailyBoxes}</SimpleBar>
+      <SimpleBar
+        style={{ height: '100%' }}
+        scrollableNodeProps={{ ref: scrollableNodeRef }}
+      >
+        {dailyBoxes}
+      </SimpleBar>
     </section>
   )
 }
+
+/*
+We're passing down the ref as props to access SimpleBar's underlying scrollable
+div element (similar to using forwardRef I guess). This lets us set a tabIndex
+on it (via useEffect).
+*/
--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,16 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import { WeatherDataContext } from '../WeatherDataContext'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
 
 export default function HourlyForecast() {
   const { processedWeatherData } = useContext(WeatherDataContext)
+  const scrollableNodeRef = useRef(null)
 
   const { hourlyBoxes } = processedWeatherData
 
+  useEffect(() => (scrollableNodeRef.current.tabIndex = '0'), [])
+
   return (
     <section className="hourly-forecast-ribbon">
-      <SimpleBar>{hourlyBoxes}</SimpleBar>
+      <SimpleBar scrollableNodeProps={{ ref: scrollableNodeRef }}>
+        {hourlyBoxes}
+      </SimpleBar>
     </section>
   )
 }
+
+/*
+We're passing down the ref as props to access SimpleBar's underlying scrollable
+div element (similar to using forwardRef I guess). This lets us set a tabIndex
+on it (via useEffect).
+*/
